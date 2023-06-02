93b758a719ba133125d191afe40e6fee214fdb43
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-06-02 22:51:24 +0530

    Summary of restaurant app added

---
+++ Header.js
@@ -0,0 +1,22 @@
+import classes from "./Header.module.css";
+import imgToUse from ".//nick-karvounis-Ciqxn7FE4vE-unsplash.jpg";
+import HeaderButton from "./HeaderButton";
+import Summary from "./Summary";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>The Lalit's</h1>
+        <HeaderButton />
+      </header>
+      <div
+        className={classes["main-image"]}
+        //as it's a CSS file with - so we can't use it with a dot notation
+      >
+        <img src={imgToUse} alt="Table" />
+      </div>
+      <Summary />
+    </>
+  );
+};
+export default Header;
