0a58a88714442430eb2e7b87b1eea0a500d3a0e0
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-06-03 23:49:20 +0530

    Some changes made in styling, still using CSS

---
+++ OptionsToEat.js
@@ -0,0 +1,46 @@
+import classes from "./OptionsToEat.module.css";
+import Card from "../UI/Card";
+import AddItem from "../Form/AddItem";
+const OPTIONS = [
+  {
+    id: "m1",
+    dish: "Rajma Chawal",
+    extraItem: "Salad and Pickle",
+    price: 250,
+  },
+  {
+    id: "m2",
+    dish: "Masala Dosa",
+    extraItem: "Sambhar & Chutney",
+    price: 120,
+  },
+  {
+    id: "m3",
+    dish: "Dam Aloo",
+    extraItem: "With Green Chutney",
+    price: 220,
+  },
+  {
+    id: "m4",
+    dish: "White Sause Pasta",
+    extraItem: "Garlic Bread",
+    price: 500,
+  },
+];
+const OptionsToEat = () => {
+  return (
+    <Card>
+      {OPTIONS.map((meal) => (
+        <div className={classes.box}>
+          <div className={classes.name}>Item__ {meal.dish}</div>
+          <div className={classes.free}>With__ {meal.extraItem}</div>
+          <div className={classes.price}>Price__ ₹{meal.price}</div>
+          <div>
+            <AddItem />
+          </div>
+        </div>
+      ))}
+    </Card>
+  );
+};
+export default OptionsToEat;
